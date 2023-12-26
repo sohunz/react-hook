5f1d570aabc6db03d5067f3b16839a9f872d924a
sohunz <sohunily@gmail.com>
2023-12-26 08:16:11 +0700

    react hook

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+const App = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+        </div>
+    );
+};
+
+export default App;
