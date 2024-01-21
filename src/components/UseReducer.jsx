7c81bb5d3bc4ea512b30f284be8fc88bcb834023
sohunz <sohunily@gmail.com>
2024-01-21 09:58:03 +0700

    useReuducer

--- src/components/UseReducer.jsx
+++ src/components/UseReducer.jsx
@@ -1,42 +1,42 @@
 import { useReducer } from "react";
 
-
-
 const reducer = (state, action) => {
-    switch(action.type){
-        case "INCREMENT" : 
-            return {count: state.count+1, showText: state.showText}
-        case "DECREMENT" :
-            return {count: state.count-1,  showText: !state.showText}
-        default: 
-            return state
+    switch (action.type) {
+        case "INCREMENT":
+            return { count: state.count + 1, showText: state.showText };
+        case "DECREMENT":
+            return { count: state.count - 1, showText: !state.showText };
+        default:
+            return state;
     }
-}
+};
 
-
+const initialState = { count: 0, showText: true };
 
 const UseReducer = () => {
+    const [state, dispatch] = useReducer(reducer, initialState);
 
-    const [state, dispatch] = useReducer(reducer, {count: 0, showText: true})
+    console.log(state);
 
     return (
         <div>
-            <p>{count}</p>
+            <p>{state.count}</p>
             <button
                 className="border py-1 px-3 m-1"
-                onClick={() => setCount(count + 1)}
+                onClick={() => dispatch({ type: "INCREMENT" })}
             >
                 +
             </button>
             <button
                 className="border py-1 px-3 m-1"
-                onClick={() => setCount(count - 1)}
+                onClick={() => {
+                    dispatch({ type: "DECREMENT" });
+                }}
             >
                 -
             </button>
 
-            {showText && <p>Hello React JS</p>}
-
+            {state.showText && <p>Hello React JS</p>}
         </div>
     );
 };
